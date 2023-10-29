b6bd47320ddad29d646e9d82ed2e858d038ec765
Sarah <sarahvictoria230@gmail.com>
2023-10-29 18:33:47 -0300

    feat: created database using sqlite
    imported AppError at server.js and created database using sqlite

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 
+const database = require("./database/sqlite")
+const AppError = require("./utils/AppError");
 const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+database();
+
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error",
+    });
+})
 
 const PORT = 4002;
 
